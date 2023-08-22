398b5914f71d529337dd4a303fd3b375aa3fc11b
Gopesh Sharma <gopesh.sharma@gmail.com>
2023-08-22 00:58:05 -0400

    login form done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Login here!</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <h2 class="mb-4 text-center">Login</h2>
+                <div class="card">
+                    <div class="card-body">
+
+                        <form action="" class="form-group">
+                            <div class="mb-2">
+                                <label for="username">User Name</label>
+                                <input name="username" type="text" class="form-control">
+                            </div>
+                            <div class="mb-2">
+                                <label for="pwd">Password</label>
+                                <input type="password" name="pwd" class="form-control">
+                            </div>
+                            <div class="mb-2">
+                                <label for="email">User Email</label>
+                                <input type="email" class="form-control">
+                            </div>
+                            <button class="btn btn-primary mt-2" type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
